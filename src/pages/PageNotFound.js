6e9a924f5fa9a5c085777f4722dff859b16ee865
DhammapalSuradkar-123 <dhammapalsuradkar24@gmail.com>
2024-06-17 22:29:26 +0530

    changes

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,18 @@
+import pageNotFoundImg from "../IMG/404-page.png";
+import "../css/pageNotFound.css";
+
 const PageNotFound = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <>
+      <div className="container pageNotFound">
+        <div className="row">
+          <div className="col-md-12 pnf">
+            <img src={pageNotFoundImg} alt="404 page" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PageNotFound;
